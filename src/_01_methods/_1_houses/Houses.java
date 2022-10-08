a24aab6cf8068a45b12fb0a4b799bf6f014f22f0
league <league@imac-14.attlocal.net>
2022-10-07 18:28:40 -0700

    HOUSES

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,5 +1,7 @@
 package _01_methods._1_houses;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /*
@@ -18,21 +20,21 @@ public class Houses {
 		//1. Have a robot start on the left side of the window near the bottom. 
 		
 		rob.setX(10);
-		rob.setY(300);
+		rob.setY(500);
 		rob.setPenWidth(10);		
 		//2. Draw a flat-topped house of height 100 with grass after it (see picture). 
 		
 		//3. Extract the piece of code that draws the house into a method. Draw 10 houses. 
-		drawHouse("small");
-		drawHouse(80);
-		drawHouse(100);
-		drawHouse(120);
-		drawHouse("small");
-		drawHouse(160);
-		drawHouse("small");
-		drawHouse(200);
-		drawHouse(225);
-		drawHouse(250);
+		drawPointyHouse("small", "red");
+		drawFlatHouse("large", "blue");
+		drawPointyHouse("medium", "green");
+		drawFlatHouse("large", "blue");
+		drawPointyHouse("small","red");
+		drawPointyHouse("medium","blue");
+		drawPointyHouse("small","green");
+		drawFlatHouse("large","red");
+		drawFlatHouse("large", "green");
+		drawPointyHouse("medium","blue");
 		//4. Change the method to take (int height) as a parameter. Draw 9 houses of different heights 
 
 		//5. Make the method take a String instead of an int. 
@@ -44,9 +46,9 @@ public class Houses {
 		        //“large”                 250 
 
 		//6. Make the method take a color as well as a height. The houses are drawn in that color. 
-
+		
 		//[optional] Set the scene to night by setting the background to black 
-
+		
 		//7. Give the houses peaked roofs 
 
 		//8. Extract that roof code into a method “drawPointyRoof” and create a new method: “drawFlatRoof”. 
@@ -55,21 +57,131 @@ public class Houses {
 	}
 	
 	
-	public void drawHouse(String height) {
+	public void drawFlatHouse(String height, String col) {
 		if(height.equals("small")){
-		rob.penDown();
-		rob.setSpeed(10);
-		rob.setPenColor(0,0,0);
-		rob.setAngle(0);
-		rob.move(60);
-		rob.setAngle(90);
-		rob.move(40);
-		rob.setAngle(180);
-		rob.move(60);
-		rob.setAngle(90);
-		rob.setPenColor(0, 255, 0);
-		rob.move(50);
-		} else if()
+			rob.penDown();
+			rob.setSpeed(10);
+			if(col.equals("red")) {
+				rob.setPenColor(255,0,0);
+			}else if(col.equals("green")) {
+				rob.setPenColor(0,225,0);
+			}else if(col.equals("blue")) {
+				rob.setPenColor(0,0,255);
+			}
+			rob.setAngle(0);
+			rob.move(60);
+			rob.setAngle(90);
+			rob.move(40);				
+			rob.setAngle(180);
+			rob.move(60);
+			rob.setAngle(90);
+			rob.setPenColor(0,255,0);
+			rob.move(50);
+		}else if(height.equals("medium")) {
+			rob.penDown();
+			rob.setSpeed(10);
+			if(col.equals("red")) {
+				rob.setPenColor(255,0,0);
+			}else if(col.equals("green")) {
+				rob.setPenColor(0,225,0);
+			}else if(col.equals("blue")) {
+				rob.setPenColor(0,0,255);
+			}
+			rob.setAngle(0);
+			rob.move(120);
+			rob.setAngle(90);
+			rob.move(40);
+			rob.setAngle(180);
+			rob.move(120);
+			rob.setAngle(90);
+			rob.setPenColor(0, 255, 0);
+			rob.move(50);
+		}else if(height.equals("large")) {
+			rob.penDown();
+			rob.setSpeed(10);
+			if(col.equals("red")) {
+				rob.setPenColor(255,0,0);
+			}else if(col.equals("green")) {
+				rob.setPenColor(0,225,0);
+			}else if(col.equals("blue")) {
+				rob.setPenColor(0,0,255);
+			}
+			rob.setAngle(0);
+			rob.move(250);
+			rob.setAngle(90);
+			rob.move(40);
+			rob.setAngle(180);
+			rob.move(250);
+			rob.setAngle(90);
+			rob.setPenColor(0, 255, 0);
+			rob.move(50);
+		}
+	}
+	public void drawPointyHouse(String height, String col) {
+		if(height.equals("small")){
+			rob.penDown();
+			rob.setSpeed(10);
+			if(col.equals("red")) {
+				rob.setPenColor(255,0,0);
+			}else if(col.equals("green")) {
+				rob.setPenColor(0,225,0);
+			}else if(col.equals("blue")) {
+				rob.setPenColor(0,0,255);
+			}
+			rob.setAngle(0);
+			rob.move(60);
+			rob.turn(45);
+			rob.move(20);
+			rob.turn(90);
+			rob.move(20);
+			rob.setAngle(180);
+			rob.move(60);
+			rob.setAngle(90);
+			rob.setPenColor(0,255,0);
+			rob.move(50);
+		}else if(height.equals("medium")) {
+			rob.penDown();
+			rob.setSpeed(10);
+			if(col.equals("red")) {
+				rob.setPenColor(255,0,0);
+			}else if(col.equals("green")) {
+				rob.setPenColor(0,225,0);
+			}else if(col.equals("blue")) {
+				rob.setPenColor(0,0,255);
+			}
+			rob.setAngle(0);
+			rob.move(120);
+			rob.turn(45);
+			rob.move(20);
+			rob.turn(90);
+			rob.move(20);
+			rob.setAngle(180);
+			rob.move(120);
+			rob.setAngle(90);
+			rob.setPenColor(0, 255, 0);
+			rob.move(50);
+		}else if(height.equals("large")) {
+			rob.penDown();
+			rob.setSpeed(10);
+			if(col.equals("red")) {
+				rob.setPenColor(255,0,0);
+			}else if(col.equals("green")) {
+				rob.setPenColor(0,225,0);
+			}else if(col.equals("blue")) {
+				rob.setPenColor(0,0,255);
+			}
+			rob.setAngle(0);
+			rob.move(250);
+			rob.turn(45);
+			rob.move(20);
+			rob.turn(90);
+			rob.move(20);
+			rob.setAngle(180);
+			rob.move(250);
+			rob.setAngle(90);
+			rob.setPenColor(0, 255, 0);
+			rob.move(50);
+		}
 	}
 	
 }
